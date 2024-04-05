547d1d1635746022de7ee9d67f223542bf84d2c4
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-04-05 18:06:25 +0000

    create 1-hbnb. js file and configure it

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  const selectedAmenities = {};
+
+  // Listen for changes on amenity checkboxes
+  $('input[type="checkbox"][data-id]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+
+    // Update the h4 tag with the list of selected amenities
+    updateAmenitiesList();
+  });
+
+  function updateAmenitiesList() {
+    const amenitiesArray = Object.values(selectedAmenities);
+    const amenitiesList = amenitiesArray.join(', ');
+
+    $('.amenities h4').text(amenitiesList);
+  }
+});
+
