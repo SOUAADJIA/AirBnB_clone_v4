7d8e891de3d5b61c3f67f4ab1db596224c58e793
Nickson K Rop <nickyrutto@gmail.com>
2024-04-06 06:31:09 -0700

    javascript files

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+  const selectedAmenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).closest('li').data('id');
+    const amenitName = $(this).closest('li').data('name');
+
+    if (this.checked) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete slectedAmenities[amenityId];
+    }
+
+    const amenitiesList = Object.values(selectedAmenities).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data) {
+          if (data.status === "OK") {
+                  $("#api_status").addClass("available");
+          } else {
+                  $("#api_status").removeClass("available");
+          }
+  });
   $.ajax({
 	  type: "POST",
 	  url: "http://0.0.0.0:5001/api/v1/places_search/",
