e91b8c82285e8475e22ce25b1c916870de83b39a
Nickson K Rop <nickyrutto@gmail.com>
2024-04-07 13:23:46 -0700

    fix sysntax error

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-  const selectedAmenities = {};
-
-  $('li input[type="checkbox"]').change(function () {
-    if (this.checked) {
-	    selectedAmenities[this.dataset.name] = this.dataset.id;
-    } else {
-	    delete slectedAmenities[this.dataset.name];
-    }
-    $('.amenities h4').text(Object.keys(selectedAmenities).sort().join(", "));
-  });
+	const selectedAmenities = {};
+        $('li input[type="checkbox"]').change(function () {
+                if (this.checked) {
+                        selectedAmenities[this.dataset.name] = this.dataset.id;
+                } else {
+                        delete selectedAmenities[this.dataset.name];
+                }
+                $('.amenities h4').text(Object.keys(selectedAmenities).sort().join(", "));
+        });
 });
